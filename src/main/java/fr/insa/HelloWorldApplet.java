1d7ca9739ae84dcb75742aa558cb7304ab020109
madptitprince <diackmouhamed99@gmail.com>
2026-09-16 23:48:44 +0200

    test: introduction volontaire d'une erreur de syntaxe (exercice 4.5)

--- src/main/java/fr/insa/HelloWorldApplet.java
+++ src/main/java/fr/insa/HelloWorldApplet.java
@@ -8,7 +8,7 @@ public class HelloWorldApplet extends Applet {
         (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o',
         (byte)' ', (byte)'W', (byte)'o', (byte)'r', (byte)'l', (byte)'d'
     };
-    private static final short HELLO_WORLD_LEN = 11;
+    private static final short HELLO_WORLD_LEN = 11
 
     private static final byte CLA_APPLET = (byte)0x80;
     private static final byte INS_GET_HELLO = (byte)0x01;
